dd0c422e7d53f509ff2f83f0d104f07a91923cac
Yaksh Bariya <yakshbari4@gmail.com>
2024-03-15 21:41:13 +0530

    fix pretty.fish script

---
+++ pretty.fish
@@ -0,0 +1,20 @@
+#!/usr/bin/fish
+function queue
+  if not test (count (jobs)) -lt (nproc)
+    wait
+  end
+  fish -c "$argv"
+end
+
+for file in 20*/*/*.{c,cpp}
+  queue clang-format -i $file
+end
+for file in 20*/*/*.js
+  queue prettier -w $file
+end
+for file in 20*/*/*.go
+  queue go fmt $file
+end
+for file in 20*/*/*.py
+  queue black $file
+end
--- pretty.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/fish
-clang-format -i 20*/*/*.{c,cpp}
-prettier -w 20*/*/*.js
-go fmt 20*/*/*.go
-black 20*/*/*.py
